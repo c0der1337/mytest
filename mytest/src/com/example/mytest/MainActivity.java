1536c36683cc7e0b31ac27444f2654320604031f
Patrick <Patrick@dssdds.de>
2014-05-27 22:07:40 +0200

    thats an other update in my master branch

--- mytest/src/com/example/mytest/MainActivity.java
+++ mytest/src/com/example/mytest/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		//test
+		// Update in the "masterbranch"
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
